f80a7d5e0d3de5225beca8de89c6a61a894daad0
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-12 14:27:25 +0100

    Create header.php

---
+++ jarditout/header.php
@@ -0,0 +1,28 @@
+<header>
+        <!--la cellule du haut "container fluid pour tt la page en bootstrap-->
+        <div><img src="assets/img/jarditou_logo.jpg" alt="logo jarditout" id="logo">
+            <p class=" float-right" id="ttlejardin">tout le jardin</p>
+        </div>
+        <!-- class foat-right ou center pour aligné le text-->
+        <nav id="navbar" class="navbar navbar-expand-sm navbar-dark col" style="background-color: #19D053">
+            <!--la navbar retrecit au niveau sm et pour changer la couleur de fond soit mette bg-"la couleur" sinon cree un style sur la bar-->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="tableau.html">Tableau</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="formulaire.html">Formulaire</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!--image de promotion-->
+        <img class="img-fluid col" src="assets\img\promotion.jpg" alt="promotion sur lame de terasse" id="promo">
+</header>
