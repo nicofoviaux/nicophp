df9a18e6d44bf7df9e83305ceee48330f36d603d
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-12 14:27:28 +0100

    Create index.php

---
+++ jarditout/index.php
@@ -0,0 +1,26 @@
+<?php 
+require("head.php");
+include("header.php");
+echo '<div class="row">
+        <article class="col-xl-8">
+            <h1>L\'entreprise</h1>
+            <hr>
+            <p>Notre entreprise familiale met tout son savoir-faire à votre disposition dans le domaine du jardin et du
+                paysagisme.</p>
+            <p>Créée il y a 70 ans, notre entreprise vend fleurs, arbustes, matériel à main et motorisés.</p>
+            <p>Implantés à Amiens, nous intervenons dans tout le département de la Somme : Albert, Doullens, Péronne,
+                Abbeville, Corbie</p>
+            <br>
+            <h2>Qualité</h2>
+            <p>Nous mettons à votre disposition un service personnalisé, avec 1 seul interlocuteur durant tout votre
+                projet.</p>
+            <p>Vous serez séduit par notre expertise, nos compétences et notre sérieux.</p>
+            <h2>Devis gratuit</h2>
+            <p>Vous pouvez bien sûr contacter pour de plus amples informations ou pour une demande d’intervention. Vous
+                souhaitez un devis ? Nous vous le réalisons gratuitement.</p>
+        </article>
+        <aside class="col-xl-4 d-none d-xl-block ">[COLONNE DROITE]</aside>
+    </div>';
+include("footer.php");
+require("pied.php");
+?>
