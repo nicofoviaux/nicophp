17af0d9bfa26e80fe0a593960ae690cb19e7c326
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-12 14:27:18 +0100

    Create footer.php

---
+++ jarditout/footer.php
@@ -0,0 +1,21 @@
+<footer>
+        <nav id="navbar" class="navbar navbar-expand-sm navbar-dark col" style="background-color: #19D053">
+            <!--la navbar retrecit au niveau sm et pour changer la couleur de fond soit mette bg-"la couleur" sinon cree un style sur la bar-->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="mentions légales.html">mention légal</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="horaire.html">horaires</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="plan.html">Plan du site</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+</footer>
