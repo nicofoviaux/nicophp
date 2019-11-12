0601e63646f642376e651b8a87488f031f80116d
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-12 14:27:21 +0100

    Create head.php

---
+++ jarditout/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">  
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/jarditout.css">
+    <title>acceuil jarditout</title>
+</head>
+<body>
+
