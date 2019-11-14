c642b368f835fd1ffcb17ca88dce2d522607f9cb
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-14 10:39:28 +0100

    Create detail.php

---
+++ jarditout/detail.php
@@ -0,0 +1,18 @@
+<?php
+require("head.php");
+include("header.php");
+require("bdd.php");//inclusion de la db
+$db=connexionBase();//appel la fonction de connexion
+$pro_id=$_GET["id"];//recupere la variable via GET (url)
+$requete="select * from produits where pro_id=".$pro_id;
+$result=$db->query($requete);
+$produit=$result->fetch(PDO::FETCH_OBJ);
+$result->closecursor();
+    echo '<img src="assets/img/'.$produit->pro_id.'.'.$produit->pro_photo.'">'.'</br>';
+    echo '<h2>reference:</h2>'.$produit->pro_ref.'</br>'; 
+    echo '<h2>libelle:</h2>'. $produit->pro_libelle.'</br>';
+    echo $produit->pro_description;
+    echo $produit->pro_prix;
+include("footer.php");
+require("pied.php");
+?>
