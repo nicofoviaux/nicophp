d97535a3fb4b99719016277dd3386f2030316e21
nikthekiller62 <nico.foviaux@hotmail.fr>
2019-11-14 10:39:39 +0100

    Create tableaux.php

---
+++ jarditout/tableaux.php
@@ -0,0 +1,48 @@
+<?php
+require("head.php");
+include("header.php");
+require('bdd.php');
+$db=connexionBase();//appel la fonction de connexion
+$requete="SELECT * FROM produits ORDER BY pro_id ASC";
+$result=$db->query($requete);
+if(!$result){
+    $tableauErreur=$db->errorinfo();
+    echo$tableauErreur[2];
+    die("Erreur dans la requête");
+}
+if($result->rowCount()==0){
+    die("la table est vide");
+}
+echo '<div class="table-responsive">
+<table class="table-bordered col">
+ <thead>
+    <tr>
+        <th scope="col"><b>Photo</b></th>
+        <th scope="col"><b>ID</b></th>
+        <th scope="col"><b>Catégorie</b></th>
+        <th scope="col"><b>Libellé</b></th>
+        <th scope="col"><b>Prix</b></th>
+        <th scope="col"><b>Couleur</b></th>
+        <th scope="col"><b>Ajout</b></th>
+        <th scope="col"><b>Modif</b></th>
+        <th scope="col"><b>Bloqué</b></th>
+    </tr>
+</thead>';
+while($row=$result->fetch(PDO::FETCH_OBJ)){
+  echo"<tr>";
+    echo "<td scope='row'><img class='visuel img-thumbnail img-fluid rounded' src='assets/img/".$row->pro_id.'.'.$row->pro_photo."'></td>\n";
+    echo"<td>".$row->pro_id."</td>";
+    echo"<td>".$row->pro_ref."</td>";
+    echo"<td><a href=\"detail.php?id=".$row->pro_id."\">".$row->pro_libelle."</a></td>";
+    echo"<td>".$row->pro_prix."</td>";
+    echo "<td>".$row->pro_couleur."</td>";
+    echo"<td>".$row->pro_d_ajout."</td>";
+    echo"<td>".$row->pro_d_modif."</td>";
+    echo"<td>".$row->pro_bloque."</td>";
+    echo"</tr>";
+}
+echo"</table>
+</div> ";
+include("footer.php");
+require("pied.php");
+?>
