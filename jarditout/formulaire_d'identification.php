1db2a2260042dc0f61c168059adaccae37d3949e
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2019-11-18 16:43:54 +0100

    Create formulaire_d'identification.php

---
+++ jarditout/formulaire_d'identification.php
@@ -0,0 +1,16 @@
+<div class="text-center">
+<form action="POST" class="form-singin">
+    <img src="assets/img/jarditou_logo.jpg" alt="logo jarditout" class="mb-4 img-fluid" width="200">
+    <h2>connexion</h2>
+    <div>
+        <label for="nom" class="sr-only">nom</label>
+        <input  type="text" id="nom" placeholder="nom d'utilisateur" require>
+    </div>
+    <div>
+        <label for="mot_de_passe" class="sr-only">mot de passe</label>
+        <input  id="mot_de_passe" required="" type="password" placeholder="mot de passe">
+    </div>
+</br>
+    <input type="button" value="connexion">
+</div>
+</form>
