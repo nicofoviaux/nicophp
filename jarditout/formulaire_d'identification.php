03c866fbb3b7b9aa744bd3baff525d67165c9b12
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2019-11-20 08:15:07 +0100

    Update formulaire_d'identification.php

--- jarditout/formulaire_d'identification.php
+++ jarditout/formulaire_d'identification.php
@@ -1,16 +1,16 @@
-<div class="text-center">
-<form action="POST" class="form-singin">
+<div class="text-center identification">
+<form action="controle.php" method="POST" class="form-singin">
     <img src="assets/img/jarditou_logo.jpg" alt="logo jarditout" class="mb-4 img-fluid" width="200">
     <h2>connexion</h2>
     <div>
         <label for="nom" class="sr-only">nom</label>
-        <input  type="text" id="nom" placeholder="nom d'utilisateur" require>
+        <input name="identifiant" type="text" id="nom" placeholder="nom d'utilisateur" require>
     </div>
     <div>
         <label for="mot_de_passe" class="sr-only">mot de passe</label>
-        <input  id="mot_de_passe" required="" type="password" placeholder="mot de passe">
+        <input  id="mot_de_passe" name="mot_de_passe" required="" type="password" placeholder="mot de passe">
     </div>
 </br>
-    <input type="button" value="connexion">
+<button class="btn btn-primary " type="submit" name="envoyer">connexion</button>
 </div>
 </form>
