4208b6b04520521dee7ba4571f5686e93fae61c3
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2019-11-20 08:15:17 +0100

    Create modif.php

---
+++ jarditout/modif.php
@@ -0,0 +1,12 @@
+<?php
+require("bdd.php");
+$db=connexionadmin("nikthekiller","PU5aqu962");
+
+if(isset($_POST["ajout"])){
+$id=$_POST[];
+$categorie=$_POST[];
+$prix=$_POST[];
+$couleur=$_POST[];
+$id=$_POST[],$_POST[];//recuperation des variable ^^
+}
+?>
