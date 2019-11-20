d9342b7af2385a36f622f84f59d9caf765d13444
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2019-11-20 08:14:50 +0100

    Update bdd.php

--- jarditout/bdd.php
+++ jarditout/bdd.php
@@ -9,4 +9,17 @@ function connexionBase(){
         die("connexion au serveur impossible");
     }
 }
+function connexionadmin($identifiant,$motdepasse){
+    try{
+        $db=new PDO('mysql:host=localhost;charset=utf8;dbname=jarditou',$identifiant,$motdepasse);
+        $connexion=true;
+        return $db;
+    }catch(exeption $e){
+        echo "Erreur: ".$e->getmessage()."</br>";
+        echo  "N°: ".$e->getcode();
+        die("connexion au serveur impossible");
+        $connexion=false;
+        
+    }
+}
 ?>
