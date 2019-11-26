2bf750344019a23b1764d81f4626ed49e6e883f7
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2019-11-26 14:15:59 +0100

    Update modif.php

--- jarditout/modif.php
+++ jarditout/modif.php
@@ -25,19 +25,40 @@ if(isset($_POST["ajout"])){
     $modif=null;
     var_dump($id,$reference,$categorie,$libelle,$descrip,$prix,$stock,$couleur,$photo,$date,$modif,$bloque);
     // securité en Cas d'oublie
-    if(empty($id)){ echo'id vide</br>';}
-    if(empty($categorie)){echo 'categorie vide </br>';}
-    if(empty($libelle)){echo 'libelle vide</br>';}
-    if(empty($prix)){echo 'prix vide</br>';}
-    if(empty($stock)){echo 'stock vide</br>';}
-    if(empty($photo)){echo 'photo vide</br>';}
+    if(empty($id)){ echo'id vide</br>';  
+    }elseif(empty($categorie)){echo 'categorie vide </br>';
+    }elseif(empty($libelle)){echo 'libelle vide</br>';
+    }elseif(empty($prix)){echo 'prix vide</br>';
+    }elseif(empty($stock)){echo 'stock vide</br>';
+    }elseif(empty($photo)){echo 'photo vide</br>';}
         else{
             //definition de la requète
-        $requete='INSERT INTO `produits` (`pro_id`, `pro_cat_id`, `pro_ref`, `pro_libelle`, `pro_description`, `pro_prix`, `pro_stock`, `pro_couleur`, `pro_photo`, `pro_d_ajout`, `pro_d_modif`, `pro_bloque`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)';
-        $q = $db->prepare($requete);
-        $q->execute(array($id,$reference,$categorie,$libelle,$descrip,$prix,$stock,$couleur,$photo,$date,$modif,$bloque));  
-     header("location:admin.php");
-        }
+    $requete='INSERT INTO `produits` (`pro_id`, `pro_cat_id`, `pro_ref`, `pro_libelle`, `pro_description`, `pro_prix`, `pro_stock`, `pro_couleur`, `pro_photo`, `pro_d_ajout`, `pro_d_modif`, `pro_bloque`) VALUES (:id,:cat,:ref,:libelle,:descrip,:prix,:stock,:couleur,:photo,:ajout,:modif,:bloque)';
+    $q = $db->prepare($requete);
+    $q -> bindValue(":id",$id,PDO::PARAM_INT);
+    $q -> bindValue(":cat",$reference,PDO::PARAM_INT);
+    $q -> bindValue(":ref",$categorie,PDO::PARAM_STR);
+    $q -> bindValue(":libelle",$libelle,PDO::PARAM_STR);
+    $q -> bindValue(":descrip",$descrip,PDO::PARAM_STR);
+    $q -> bindValue(":prix",$prix,PDO::PARAM_INT);
+    $q -> bindValue(":stock",$stock,PDO::PARAM_INT);
+    $q -> bindValue(":couleur",$couleur,PDO::PARAM_STR);
+    $q -> bindValue(":photo",$photo,PDO::PARAM_STR);
+    $q -> bindValue(":ajout",$date,PDO::PARAM_STR);
+    $q -> bindValue(":modif",$modif,PDO::PARAM_STR);
+    $q -> bindValue(":bloque",$bloque,PDO::PARAM_INT);
+    $q->execute();  
+}
+    if(!$q){
+        $tableauErreur=$db->errorinfo();
+        echo$tableauErreur[2];
+        die("Erreur dans la requête");
+        var_dump($id,$reference,$categorie,$libelle,$descrip,$prix,$stock,$couleur,$photo,$date,$modif,$bloque);
+    }else{
+
+        header("location:admin.php");
+    }
+     
     }
 //----------------------------------------------------------------------------MAJ
 if(isset($_POST["modif"])){
@@ -57,12 +78,39 @@ if(isset($_POST["modif"])){
     $bloque=intval($_POST["bloque"]);
     $date=$donee->pro_d_ajout;//va les cherche directement en bdd
     $modif=date('Y-m-d H:i:s');
+
+    if(empty($id)){ echo'id vide</br>';
+    }elseif(empty($categorie)){echo 'categorie vide </br>';
+    }elseif(empty($libelle)){echo 'libelle vide</br>';
+    }elseif(empty($prix)){echo 'prix vide</br>';
+    }elseif(empty($stock)){echo 'stock vide</br>';
+    }elseif(empty($photo)){echo 'photo vide</br>';}
+        else{
     // requete
-    $Maj='UPDATE `produits` SET pro_id=?,pro_cat_id=?,pro_ref=?,pro_libelle=?,pro_description=?,pro_prix=?,pro_stock=?,pro_couleur=?,pro_photo=?,pro_d_ajout=?,pro_d_modif=?,pro_bloque=? WHERE `produits`.`pro_id` = '.$id;
+    $Maj='UPDATE `produits` SET pro_id=:id,pro_cat_id=:cat,pro_ref=:ref,pro_libelle=:libelle,pro_description=:descrip,pro_prix=:prix,pro_stock=:stock,pro_couleur=:couleur,pro_photo=:photo,pro_d_ajout=:ajout,pro_d_modif=:modif,pro_bloque=:bloque WHERE `produits`.`pro_id` = '.$id;
     $q = $db->prepare($Maj);
-    $q->execute(array($id,$reference,$categorie,$libelle,$descrip,$prix,$stock,$couleur,$photo,$date,$modif,$bloque)); 
-    var_dump($id,$reference,$categorie,$libelle,$descrip,$prix,$stock,$couleur,$photo,$date,$modif,$bloque);
-    header("location:admin.php");
+    $q -> bindValue(":id",$id,PDO::PARAM_INT);
+    $q -> bindValue(":cat",$reference,PDO::PARAM_INT);
+    $q -> bindValue(":ref",$categorie,PDO::PARAM_STR);
+    $q -> bindValue(":libelle",$libelle,PDO::PARAM_STR);
+    $q -> bindValue(":descrip",$descrip,PDO::PARAM_STR);
+    $q -> bindValue(":prix",$prix,PDO::PARAM_INT);
+    $q -> bindValue(":stock",$stock,PDO::PARAM_INT);
+    $q -> bindValue(":couleur",$couleur,PDO::PARAM_STR);
+    $q -> bindValue(":photo",$photo,PDO::PARAM_STR);
+    $q -> bindValue(":ajout",$date,PDO::PARAM_STR);
+    $q -> bindValue(":modif",$modif,PDO::PARAM_STR);
+    $q -> bindValue(":bloque",$bloque,PDO::PARAM_INT);
+    $q->execute(); 
+        }
+    if(!$q){
+        $tableauErreur=$db->errorinfo();
+        echo$tableauErreur[2];
+        die("Erreur dans la requête");
+        var_dump($id,$reference,$categorie,$libelle,$descrip,$prix,$stock,$couleur,$photo,$date,$modif,$bloque);
+    }else{
+        header("location:admin.php");
+    }
 }
 //----------------------------------------------------------------------------SUPRESSION
 if(isset($_POST["supprime"])){
