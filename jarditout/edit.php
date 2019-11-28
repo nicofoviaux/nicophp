5ce43e52e8ba456cfb6d717fd52483e05e52e83c
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2019-11-28 10:33:51 +0100

    Delete edit.php

--- jarditout/edit.php
+++
@@ -1,8 +0,0 @@
-<?php
-require('database.php');
-if($_SERVEUR["REQUEST_METHOD"]=="GET" && !empty($_GET)){
-$sql= "INSERT INTO  `produits` (pro_cat_id,pro_ref,pro_libelle,pro_description, pro_couleur,pro_photo,pro_d_ajout, pro_d_modif, pro_bloque) VALUE ($categorie,$ref,$libelle,$description,$couleur,$photo,$ajout,$modif,$bloqué)";
-$PDO->prepare($sql);
-}
-
-?>
