c57f4e9e2c723759085f8167e2fa00ecc8e5d749
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2019-11-28 10:33:36 +0100

    Update controle.php

--- jarditout/controle.php
+++ jarditout/controle.php
@@ -1,14 +1,27 @@
 <?php
+/*
+----------------------------CONTROLE DE L IDENTIFICATION--------------------------
+->identification admin
+   *verif des autorisation de modif de BDD 
+   *connection a la session admin 
+->identification client 
+    *reste a faire
+*/
+session_start();
+
 require('bdd.php');
 if(isset($_POST["envoyer"])){
+    
     $identifiant=$_POST["identifiant"];
     $motdepasse=$_POST["mot_de_passe"];
-    $db=connexionadmin($identifiant,$motdepasse);
-    if($db!=0){
-        header('Location: admin.php');
-    }else{
-        echo "erreur de connexion";
-    }
-    var_dump($db);
+    list($db,$connexion)=connexionadmin($identifiant,$motdepasse);
+    var_dump($connexion);
+    if($connexion==true)
+    {
+         $_SESSION["connecte"] = 'nico'; 
+         var_dump($_SESSION);       
+        header('Location:admin.php');
+        exit; 
+      }
 }
 ?>
