eb4544e3d8e4e747b457162bd6c71fa0552f666a
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2019-11-28 10:34:06 +0100

    Update head.php

--- jarditout/head.php
+++ jarditout/head.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://kit.fontawesome.com/1e7dc74d63.js"></script>
     <link rel="stylesheet" href="assets/css/jarditout.css">
     <title>jarditou</title>
 </head>
