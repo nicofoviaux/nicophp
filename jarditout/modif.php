2c00cfd83d70e8d0e0badbc9d8472c3821eefd15
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2019-11-28 16:49:30 +0100

    Update modif.php

--- jarditout/modif.php
+++ jarditout/modif.php
@@ -6,30 +6,57 @@ MODIFICATION DE LA BASE DE DONNEE AVEC LE SITE
 -requete de MAJ 2nd position
 -requete de supression 3eme position
 
+----------------------------test de securité-----------------------
+
+->voir si les variables sont ok 
+->testé le fichier image 
+->enregistré la photo des assets/img/$pro_id.extension
+
 */ 
     require("bdd.php");
     $db=connexionBase();
+    $filtrestock='/^[0-9]$/';
+    $dossier='assets/img';
 //---------------------------------------------------------------------------------------------------AJOUT
 if(isset($_POST["ajout"])){
-    $id=intval($_POST["id"]);// pour avoir la variable en int
+    $id= (int) $_POST["id"];
     $categorie=$_POST["categorie"];
-    $reference=intval($_POST["reference"]);
+    $reference=(int) $_POST["reference"];
     $libelle=$_POST["libelle"];
     $descrip=$_POST["description"];
-    $prix=floatval($_POST["prix"]);// pour avoir la variable en decimal
-    $stock=intval($_POST["stock"]);
+    $prix=(float) $_POST["prix"]; // pour avoir la variable en decimal
+    $stock=$_POST["stock"];
+    if(!preg_match($filtrestock,$stock)){
+    echo "format incorrect </br>" ;
+    }else{
+        $stock = (int)$stock;
+    }
     $couleur=$_POST["couleur"];
-    $photo=$_POST["photo"];
-    $bloque=intval($_POST["bloque"]);
+    if (is_array($_FILES["fichier"]["error"]) && !empty($_FILES["fichier"]["error"])){ 
+      
+                    
+      
+    } 
+    $aMimeType=array("image/gif", "image/jpeg", "image/pjpeg", "image/png", "image/x-png", "image/tiff");
+    $finfo=finfo_open(FILEINFO_MIME_TYPE);
+    $mimeType=finfo_file($finfo,$_FILES["photo"]["tmp_name"]);
+    var_dump($_FILES);
+    if(in_array($mimeType,$aMimeType)){
+        $extension= substr(strrchr($_FILES["photo"]["name"], "."), 1);
+        move_uploaded_file($_FILES["photo"]["tmp_name"], $dossier.'/'.$id.'.'.$extension); 
+    }else{
+      echo"extension du fichie incorrect";
+    }
+
+    $bloque=(int) $_POST["bloque"];
     $date=date('Y-m-d');//prend la date d'aujourd'hui le "-" pour compatibilité sql
     $modif=null;
-    var_dump($id,$reference,$categorie,$libelle,$descrip,$prix,$stock,$couleur,$photo,$date,$modif,$bloque);
+    var_dump($id);
     // securité en Cas d'oublie
     if(empty($id)){ echo'id vide</br>';  
     }elseif(empty($categorie)){echo 'categorie vide </br>';
     }elseif(empty($libelle)){echo 'libelle vide</br>';
     }elseif(empty($prix)){echo 'prix vide</br>';
-    }elseif(empty($stock)){echo 'stock vide</br>';
     }elseif(empty($photo)){echo 'photo vide</br>';}
         else{
             //definition de la requète
@@ -56,13 +83,13 @@ if(isset($_POST["ajout"])){
         var_dump($id,$reference,$categorie,$libelle,$descrip,$prix,$stock,$couleur,$photo,$date,$modif,$bloque);
     }else{
 
-        header("location:admin.php");
+       // header("location:admin.php");
     }
      
     }
 //----------------------------------------------------------------------------MAJ
 if(isset($_POST["modif"])){
-    $id=intval($_POST["var"]);
+    $id=(int) $_POST["var"];
     $sql2='SELECT * FROM `produits` WHERE pro_id='.$id;
     $result=$db->query($sql2);
     $donee=$result->fetchObject();
@@ -71,11 +98,16 @@ if(isset($_POST["modif"])){
     $reference=$donee->pro_cat_id;
     $libelle=$_POST["libelle"];
     $descrip=$_POST["description"];
-    $prix=floatval($_POST["prix"]);
-    $stock=intval($_POST["stock"]);
+    $prix=(float) $_POST["prix"];
+    $stock=(int)$_POST["stock"];
+    if(!preg_match($filtrestock,$stock)){
+        echo "format incorrect </br>" ;
+        }else{
+            $stock = (int)$stock;
+        }
     $couleur=$_POST["couleur"];
     $photo=$_POST["photo"];
-    $bloque=intval($_POST["bloque"]);
+    $bloque=(int )$_POST["bloque"];
     $date=$donee->pro_d_ajout;//va les cherche directement en bdd
     $modif=date('Y-m-d H:i:s');
 
@@ -83,7 +115,6 @@ if(isset($_POST["modif"])){
     }elseif(empty($categorie)){echo 'categorie vide </br>';
     }elseif(empty($libelle)){echo 'libelle vide</br>';
     }elseif(empty($prix)){echo 'prix vide</br>';
-    }elseif(empty($stock)){echo 'stock vide</br>';
     }elseif(empty($photo)){echo 'photo vide</br>';}
         else{
     // requete
