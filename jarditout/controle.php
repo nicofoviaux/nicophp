2eae6f5d5b3ef7297857c307afbb23632c27feda
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2019-12-06 09:44:27 +0100

    Update controle.php

--- jarditout/controle.php
+++ jarditout/controle.php
@@ -1,27 +1,39 @@
 <?php
 /*
 ----------------------------CONTROLE DE L IDENTIFICATION--------------------------
-->identification admin
-   *verif des autorisation de modif de BDD 
-   *connection a la session admin 
 ->identification client 
-    *reste a faire
-*/
-session_start();
 
-require('bdd.php');
-if(isset($_POST["envoyer"])){
+*/
     
+    session_start();
+    require('bdd.php');
+    $db=connexionBase();
+if(isset($_POST["envoyer"])){
+
+    $mail=$_POST["mail"];
     $identifiant=$_POST["identifiant"];
-    $motdepasse=$_POST["mot_de_passe"];
-    list($db,$connexion)=connexionadmin($identifiant,$motdepasse);
-    var_dump($connexion);
-    if($connexion==true)
-    {
-         $_SESSION["connecte"] = 'nico'; 
-         var_dump($_SESSION);       
-        header('Location:admin.php');
-        exit; 
-      }
+    $password= $_POST["mot_de_passe"];
+    $requete='SELECT * FROM clients where cli_identifiant = "'.$identifiant.'"';
+    $result=$db->query($requete);
+    $donee=$result->fetchObject();
+
+    var_dump($donee->cli_identifiant,$identifiant);
+    if($donee->cli_mail != $mail){
+        header("Location:id.php?error=1");
+        exit;
+    }else if($donee->cli_identifiant != $identifiant){
+        header("Location:id.php?error=2");
+        exit;
+    }elseif(password_verify($password,$donee->cli_MDP)){
+        $_SESSION["connecte"]='client';
+        header("Location:index.php?account='".$identifiant."'");
+        exit;
+    }else{
+        var_dump(password_verify($password,$donee->cli_MDP));
+       header("Location:id.php?error=3");
+        exit;
+    }   
 }
+
+
 ?>
