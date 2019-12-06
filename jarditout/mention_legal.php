dd0fa2bbae3b5d3aaf1a28553b24d2548b7dcbc7
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2019-12-06 10:40:26 +0100

    Update mention_legal.php

--- jarditout/mention_legal.php
+++ jarditout/mention_legal.php
@@ -6,11 +6,45 @@
 require("head.php");
 include("header.php");
 ?>
-<section>
-<article class="text-center">
-  <img src="assets\img\site_en_construction.gif" alt="page en developpement" sizes="200%">
+<section class="text-center" >
+<h1><u>Mention Légales</u></h1>
+<article>
+<hr>
+<h2>vente</h2>
+<p>ce site est purement fictif dans un but d'un entrainement à la programmation</p>
+<p> aucune vente ne pourras être realisé de même que les prix renseigné sont fictifs </p>
+<h2>livraison</h2>
+<p>aucune livraison ne sera effectué </p>
+<h2>payement/remboursement</h2>
+<p>aucun payement vous sera demandé et de ce fait aucun remboursement ne pourra ce faire </p> 
+</article>
+<br>
+<article>
+<h1><u>Hebergement</u></h1>
+<p>tryte</p>
+<p>htht</p>
+</article>
+<br>
+<article>
+<h1><u>Obligations liées à la transparence de l'information</u></h1>
+</article>
+<br>
+<article>
+<h1><u>Politique de protection des données personnelles (RGPD)</u></h1>
+</article>
+<br>
+<article>
+<h1><u>Respecter le code de la propriété intellectuelle</u></h1>
+</article>
+<br>
+<article>
+<h1><u>Respecter le droit à l’image</u></h1>
 </article>
 </section>
+<div class=text-right>
+<a href="idadmin.php">l'equipe</a>
+</div>
+
 <?php
 include("footer.php");
 require("pied.php");
