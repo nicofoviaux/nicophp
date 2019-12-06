d105ddf13f8744639a5c13f6cbbd7adafeda3ed9
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2019-12-06 09:44:51 +0100

    Update id.php

--- jarditout/id.php
+++ jarditout/id.php
@@ -10,6 +10,24 @@
 ->/!\ SECURITE /!\
 */
 require("head.php");
+$erreur=isset($_GET["error"]);
+$mail="&#xf0e0; Adresse Mail";
+$identifiant="&#xf007; Nom d'Utilisateur";
+$mdp="&#xf084; Mot de Passe";
+
+switch($erreur){
+    case 0;
+break;
+    case 1;
+    $mail="&#xf071; email incorrect";
+break;
+    case 2;
+    $identifiant="&#xf071; identifiant incorrect";
+break;
+    case 3;
+    $mdp="&#xf071; Mot de Passe incorrect";
+break;
+}
 ?>
 <div class="text-center identification ">
 <fieldset class= "d-inline-flex p-2 ">
@@ -18,15 +36,15 @@ require("head.php");
     <h2>connexion</h2>
     <div>
         <label for="mail" class="sr-only"></label>
-        <input class="fontAwesome" type="email" name="mail" id="mail" placeholder="&#xf0e0; Adresse Mail"  >
+        <input class="fontAwesome" type="email" name="mail" id="mail" placeholder= "<?=$mail?>" >
      </div>
      <div>
         <label for="nom" class="sr-only">nom</label>
-        <input class="fontAwesome" name="identifiant" type="text" id="nom" placeholder="&#xf007; Nom d'Utilisateur" required>
+        <input class="fontAwesome" name="identifiant" type="text" id="nom" placeholder="<?=$identifiant?>" required>
         </div>
     <div>
         <label for="mot_de_passe" class="sr-only">mot de passe</label>
-        <input class="fontAwesome"  id="mot_de_passe" name="mot_de_passe" required type="password" placeholder="&#xf084; Mot de Passe">
+        <input class="fontAwesome"  id="mot_de_passe" name="mot_de_passe" required type="password" placeholder="<?=$mdp?>">
     </div>
     <br>
     <p>vous n'avez pas encore de compte? <a href="formulaireInscription.php?error=0"> clicquez ici !</a></p>
